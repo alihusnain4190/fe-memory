cb54ba00ed9c5aec7c53d8846bdf9b81854bf385
alihusnain4190 <alihusnain4190@gmail.com>
2020-11-19 14:55:35 +0000

    add header navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Router } from "@reach/router";
-import Landing from './pages/Landing'
-const  App=() =>(
-  <Router>
-    <Landing path="/"></Landing>
-  </Router>
-)
+import Landing from "./pages/Landing";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const App = () => (
+  <div>
+    <Header />
+    <Navbar />
+    <Router>
+      <Landing path="/"></Landing>
+    </Router>
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <h2>Family Memory</h2>
+    </div>
+  );
+};
+
+export default Header;
